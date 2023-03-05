4d6c6a91d804a53951ee5a16f0bd9448a2c93ff2
bekatadesse <bekatadesse5@gmail.com>
2023-03-05 06:07:10 +0000

    update

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -21,20 +21,22 @@ babel = Babel(app)
 @app.route('/')
 def index():
     """view function for root route
+
     Returns:
         html: homepage
     """
     return render_template('2-index.html')
 
 
+@babel.localeselector
 def get_locale():
     """get best language match
+
     Returns:
         str: best match
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-babel.init_app(app, locale_selector=get_locale)
 
 if __name__ == '__main__':
     app.run()
