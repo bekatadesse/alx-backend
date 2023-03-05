b1a0bcb1f213a57fac5a159fca4ed0b82950c1f8
bekatadesse <bekatadesse5@gmail.com>
2023-03-05 05:27:19 +0000

    update3

--- 0x02-i18/0-app.py
+++ 0x02-i18/0-app.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
-"""
-Module 0-app
-"""
+"""Basic Flask app"""
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+app.debug = True
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    """didplays a basic hello world message"""
-    return render_template("0-index.html")
+    """view function for root route
+    Returns:
+        html: homepage
+    """
+    return render_template('0-index.html')
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
