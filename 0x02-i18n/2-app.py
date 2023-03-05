832df734234b93a59b103f0d9068ba15035c3a6a
bekatadesse <bekatadesse5@gmail.com>
2023-03-05 06:01:21 +0000

    task 2 update

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,35 +1,40 @@
 #!/usr/bin/env python3
-"""
-Module 0-app
-"""
+"""Basic Babel Setup"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-app = Flask(__name__)
-babel = Babel(app)
 
-
-class Config(object):
-    """i18n configuration"""
+class Config:
+    """Config class for flask app
+    """
+    DEBUG = True
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
+babel = Babel(app)
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    """didplays a basic hello world message"""
-    return render_template("2-index.html")
+    """view function for root route
+    Returns:
+        html: homepage
+    """
+    return render_template('2-index.html')
 
 
+@babel.localeselector
 def get_locale():
-    """Gets best fmatch locale according to request"""
-    return request.accept_languages.best_match(Config.LANGUAGES)
+    """get best language match
+    Returns:
+        str: best match
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-babel.init_app(app, locale_selector=get_locale)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
