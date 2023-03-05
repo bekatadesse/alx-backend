6ef6d3104d3d2ecf8bb95758325e9b9cfada17a8
bekatadesse <bekatadesse5@gmail.com>
2023-03-05 06:03:53 +0000

    update 3

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -27,7 +27,6 @@ def index():
     return render_template('2-index.html')
 
 
-@babel.localeselector
 def get_locale():
     """get best language match
     Returns:
@@ -35,6 +34,7 @@ def get_locale():
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+babel.init_app(app, locale_selector=get_locale)
 
 if __name__ == '__main__':
     app.run()
